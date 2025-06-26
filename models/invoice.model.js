6c08f495ccc3421cb5d118b0d99cccada9bfbada
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-06-26 14:24:08 -0300

    .

--- models/invoice.model.js
+++ models/invoice.model.js
@@ -34,7 +34,7 @@ module.exports = (sequelize, DataTypes) => {
                 }
             },
             book_id: {
-                type: DataTypes.BIGINT,
+                type: DataTypes.STRING,
                 allowNull: false,
                 references: {
                     model: 'books',
